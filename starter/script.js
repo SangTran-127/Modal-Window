5f13b72aa66d5e05fc3ab95804818def88016720
Trần Quang Sáng <76774572+SangTran-127@users.noreply.github.com>
2021-01-03 20:27:37 +0700

    Add files via upload

---
+++ starter/script.js
@@ -0,0 +1,24 @@
+'use strict';
+const hidden = document.querySelector('.hidden');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const close = document.querySelector('.close-modal');
+const show_modal = document.querySelectorAll('.show-modal');
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+for (let i = 0; i < show_modal.length; i++) {
+  show_modal[i].addEventListener('click', openModal);
+}
+close.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (btn) {
+  if (btn.key === 'Escape') {
+    closeModal();
+  }
+});
